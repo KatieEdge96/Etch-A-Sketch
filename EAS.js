b180c78f6b1bcfa4fc50c08b523060484168bb6f
Katie Edge <K.L.Edge@hotmail.com>
2022-09-22 17:28:31 +0100

    Satrted on button functions and some CSS

--- EAS.js
+++ EAS.js
@@ -9,10 +9,25 @@ function createDivs(col, rows) {
     for(let i = 0 ; i < (col * rows); i++){
         const div = document.createElement('div')
         div.style.border = '1px solid red'
-        container.style.gridTemplateColumns = `repeat(${col}, ifr)`;
-        container.style.gridTemplateRows = `repeat(${rows}, ifr)`;
+        container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
         container.appendChild(div).classList.add('box')
     }
 }
 
 createDivs(16,16)
+
+function greyColor() {
+    const boxs = container.querySelectorAll('.box')
+    btnGrey.textContent = 'Grey'
+    btnGrey.addEventListener('click' , () =>{
+        boxs.forEach(box => box.addEventListener('mouseover', () => {
+            let Rnum = Math.floor(Math.random() * 255)
+            box.style.background = `rgb(${Rnum},${Rnum},${Rnum})`
+        }))
+
+    })
+
+    buttonsContainer.appendChild(btnGrey).classList.add('btn')
+}
+greyColor()
